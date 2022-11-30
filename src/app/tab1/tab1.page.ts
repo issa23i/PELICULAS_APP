d4df508c07b1600834f1f140675412db3d88b7bf
issa23i <ipaslop262@iesmartinezm.es>
2022-11-30 13:02:20 +0100

    swiper

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+
+
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit {
 
-  constructor() {}
+  constructor() { }
+
+
+  arrayPeliculas: any[] = Array(0);
+  ngOnInit() {
+    for (let i = 1; i < 21; i++) {
+      this.arrayPeliculas.push(`Pelicula ${i}`);
+    }
+  }
 
 }
