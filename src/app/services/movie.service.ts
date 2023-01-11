bdc7fcb2b2d3fc307a6f983b74ddf58b8f51f8c8
issa23i <ipaslop262@iesmartinezm.es>
2023-01-11 13:01:14 +0100

    corregido fechas ultimas peliculas

--- src/app/services/movie.service.ts
+++ src/app/services/movie.service.ts
@@ -10,6 +10,6 @@ export class MovieService {
   constructor( private http: HttpClient) {}
 
   getFeature(){
-    return this.http.get<RespuestaMDB>('https://api.themoviedb.org/3/discover/movie?primary_release_date.gte=2022-11-01&primary_release_date.lte=2023-01-11&api_key=09a5b4a4ca15c3d268a44d5d5fb83522&language=es&include_images_language=es');
+    return this.http.get<RespuestaMDB>('https://api.themoviedb.org/3/discover/movie?primary_release_date.gte=2021-09-01&primary_release_date.lte=2021-11-15&api_key=09a5b4a4ca15c3d268a44d5d5fb83522&language=es&include_images_language=es');
   }
 }
