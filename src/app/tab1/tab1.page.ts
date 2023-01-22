7a387ff0e6ff21d7d3df295ddf60170556b0a5cf
issa23i <ipaslop262@iesmartinezm.es>
2023-01-22 15:03:03 +0100

    quitar opciones (ya puestas en swiper)

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -17,12 +17,12 @@ SwiperCore.use([Autoplay, Keyboard, Pagination, Scrollbar, Zoom, IonicSlides]);
 export class Tab1Page implements OnInit {
 
   peliculasRecientes: Pelicula[] = [];
-  
+  /** 
   slidesOpt = {
     slidesPerView : 1.1,
     freemode : true
   };
-
+*/
   constructor( private movieService : MovieService) { }
 
 
