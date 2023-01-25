0b978afb4920a13d472e6f31135481b3ca7b2393
issa23i <ipaslop262@iesmartinezm.es>
2023-01-25 11:16:16 +0100

    Detalle componemt arreglado

--- src/app/component/detalle/detalle.component.ts
+++ src/app/component/detalle/detalle.component.ts
@@ -1,5 +1,9 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular/providers/modal-controller';
+import { Cast, PeliculaDetalle } from 'src/app/interfaces/interfaces';
 import { CLIENT_RENEG_LIMIT } from 'tls';
+
+import { MovieService } from '../../services/movie.service';
 
 @Component({
   selector: 'app-detalle',
@@ -10,10 +14,42 @@ export class DetalleComponent implements OnInit {
   
   @Input() id: any;
 
-  constructor() { }
+  pelicula: PeliculaDetalle = {};
+  actores: Cast[] = [];
+  oculto = 150;
+  estrella = 'star-outline';
 
-  ngOnInit() {
-    console.log("ID: ",this.id)
-  }
+  slideOptActores = {
+    slidesPerView: 3.3,
+    freeMode: true,
+    spaceBetween: 0
+  };
 
+
+
+
+  constructor( private movieService: MovieService,
+    private modalCtrl: ModalController ) { }
+
+ngOnInit() {
+// console.log('ID', this.id );
+
+
+this.movieService.getPeliculaDetalle( this.id )
+.subscribe( resp => {
+console.log( resp );
+this.pelicula = resp;
+});
+
+this.movieService.getActoresPelicula( this.id )
+.subscribe( resp => {
+console.log( resp );
+this.actores = resp.cast;
+});
+
+}
+
+regresar() {
+this.modalCtrl.dismiss();
+}
 }
