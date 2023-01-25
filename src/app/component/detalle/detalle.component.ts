6234838e141776b1446871d5db65847fbc2a9920
issa23i <ipaslop262@iesmartinezm.es>
2023-01-25 12:28:58 +0100

    arreglado component module ts - Estable

--- src/app/component/detalle/detalle.component.ts
+++ src/app/component/detalle/detalle.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { ModalController } from '@ionic/angular/providers/modal-controller';
+import { ModalController } from '@ionic/angular';
 import { Cast, PeliculaDetalle } from 'src/app/interfaces/interfaces';
 import { CLIENT_RENEG_LIMIT } from 'tls';
 
@@ -11,45 +11,45 @@ import { MovieService } from '../../services/movie.service';
   styleUrls: ['./detalle.component.scss'],
 })
 export class DetalleComponent implements OnInit {
-  
+
   @Input() id: any;
 
   pelicula: PeliculaDetalle = {};
   actores: Cast[] = [];
   oculto = 150;
   estrella = 'star-outline';
-
-  slideOptActores = {
-    slidesPerView: 3.3,
-    freeMode: true,
-    spaceBetween: 0
-  };
-
-
-
-
-  constructor( private movieService: MovieService,
-    private modalCtrl: ModalController ) { }
-
-ngOnInit() {
-// console.log('ID', this.id );
+  /** 
+    slideOptActores = {
+      slidesPerView: 3.3,
+      freeMode: true,
+      spaceBetween: 0
+    };
+  */
 
 
-this.movieService.getPeliculaDetalle( this.id )
-.subscribe( resp => {
-console.log( resp );
-this.pelicula = resp;
-});
 
-this.movieService.getActoresPelicula( this.id )
-.subscribe( resp => {
-console.log( resp );
-this.actores = resp.cast;
-});
+  constructor(private movieService: MovieService,
+    private modalCtrl: ModalController) { }
 
-}
+  ngOnInit() {
+    // console.log('ID', this.id );
 
-regresar() {
-this.modalCtrl.dismiss();
-}
+
+    this.movieService.getPeliculaDetalle(this.id)
+      .subscribe(resp => {
+        console.log(resp);
+        this.pelicula = resp;
+      });
+
+    this.movieService.getActoresPelicula(this.id)
+      .subscribe(resp => {
+        console.log(resp);
+        this.actores = resp.cast;
+      });
+
+  }
+
+  regresar() {
+    this.modalCtrl.dismiss();
+  }
 }
