e1e0df6e0b7f12cbeb1ce021088ff3c9cdf0a92a
issa23i <ipaslop262@iesmartinezm.es>
2023-01-25 10:44:58 +0100

    arreglado error slideshow-backdrop

--- src/app/interfaces/interfaces.ts
+++ src/app/interfaces/interfaces.ts
@@ -18,7 +18,7 @@ export interface Pelicula {
   original_language: string;
   original_title: string;
   genre_ids: number[];
-  backdrop_path?: string;
+  backdrop_path: string;
   adult: boolean;
   overview: string;
   release_date: string;
